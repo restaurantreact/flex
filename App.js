189f5206d4976c7e3c4cbcf9da35d3f749c8d0f5
rajeshraj1998 <srsrajesh5@gmail.com>
2020-07-11 17:12:19 +0530

    1st flex normal

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {View} from "react-native";
+
+export default function App(){
+  return(
+    <View style={{
+      backgroundColor:"#fff ",
+      flex:1
+    }}>
+
+    <View style={{
+      backgroundColor:"dodgerblue",
+      flex:1
+    }}></View>
+       <View style={{
+      backgroundColor:"gold",
+      flex:1
+    }}></View>
+       <View style={{
+      backgroundColor:"tomato",
+      flex:1
+    }}></View>
+
+    </View>
+
+
+  );
+}
